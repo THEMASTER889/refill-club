58c71c086fdcde4ffca58a8d246d4ef6e6ac27f5
THEMASTER889 <justinlees752@gmail.com>
2026-09-04 06:34:14 +1000

    Connect Lemon Squeezy checkout to Join buttons

--- js/config.js
+++ js/config.js
@@ -7,6 +7,6 @@
  *   https://your-store.lemonsqueezy.com/checkout/buy/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx?embed=1
  */
 window.REFILL_CLUB = {
-  checkoutUrl: "",
+  checkoutUrl: "https://refillclub.lemonsqueezy.com/checkout/buy/b8b4be09-a118-4751-bc1a-4073053f29ed?embed=1",
   priceLabel: "A$9 / month",
 };
